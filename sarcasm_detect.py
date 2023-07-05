cbfdaa431a8660d4a0f66fdabbe2052c460c250a
Muhammad40091 <129478070+Muhammad40091@users.noreply.github.com>
2023-07-05 15:58:15 +0500

    Update sarcasm_detect.py

--- sarcasm_detect.py
+++ sarcasm_detect.py
@@ -3,7 +3,6 @@ import pandas as pd
 import tensorflow as tf
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
-from sklearn.model_selection import train_test_split
 import streamlit as st
 import random
 
@@ -15,14 +14,16 @@ max_length = 100
 trunc_type = 'post'
 padding_type = 'post'
 oov_tok = "<OOV>"
-training_size = 20000
 epochs = 5
 
+# Read csv file for using pandas libary
 sarcasm_df = pd.read_csv("Data.csv")
+
+# Split them into two columns
 input_seq = sarcasm_df['headlines']
 target_seq = sarcasm_df['target']
 
-tokenizer = Tokenizer(num_words=vocab_size, oov_token=oov_tok, )
+tokenizer = Tokenizer(num_words=vocab_size, oov_token=oov_tok)
 tokenizer.fit_on_texts(input_seq)
 word_index = tokenizer.word_index
 model = tf.keras.models.load_model('sarcasm_detect.h5')
@@ -51,12 +52,11 @@ def handle_input_text():
         input_padded_sentences = pad_sequences(input_sentences, maxlen=max_length, padding=padding_type,
                                                truncating=trunc_type)
         probs = model.predict(input_padded_sentences)
-        preds = f"{int(np.round(probs))}"
+        pred_label = "Sarcastic" if probs[0][0] >= 0.5 else "Not Sarcastic"
         col3.empty()
-        if preds == '1':
-            col3.write("Sarcastic")
-        else:
-            col3.write("Not Sarcastic")
+        col3.write(pred_label)
+    else:
+        col3.empty()
 
 
 color_palette = {
@@ -73,6 +73,6 @@ selected_color = 'red'
 selected_color = st.selectbox("Select Color:", list(color_palette.keys()))
 
 # Set the color of col3 based on the selected color
-st.color_picker("Result Color", color_palette[selected_color])
+col3 = st.color_picker("Result Color", color_palette[selected_color])
 
 col2.button("Detect🔍", on_click=handle_input_text)
