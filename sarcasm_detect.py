da3498bb16f55f5c69a1689b8b0ba43bb62af15d
Muhammad40091 <129478070+Muhammad40091@users.noreply.github.com>
2023-07-05 15:44:52 +0500

    Update sarcasm_detect.py

--- sarcasm_detect.py
+++ sarcasm_detect.py
@@ -1,3 +1,4 @@
+# Import some of the most important libary for using this notebook
 import numpy as np
 import pandas as pd
 import tensorflow as tf
@@ -5,20 +6,8 @@ from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from sklearn.model_selection import train_test_split
 import streamlit as st
-import random
 
-st.title("Sarcasm Text Detection")
-
-color_palette = {
-    'red': '#FF0000',
-    'orange': '#FFA500',
-    'yellow': '#FFFF00',
-    'green': '#008000',
-    'blue': '#0000FF',
-    'purple': '#800080'
-}
-
-selected_color = 'red'
+st.title("Sarcasm Text Detection ")
 
 vocab_size = 10000
 embedding_dim = 16
@@ -29,29 +18,19 @@ oov_tok = "<OOV>"
 training_size = 20000
 epochs = 5
 
+# Read csv file for using pandas libary
 sarcasm_df = pd.read_csv("Data.csv")
+
+# Split them two column
 input_seq = sarcasm_df['headlines']
 target_seq = sarcasm_df['target']
 
-tokenizer = Tokenizer(num_words=vocab_size, oov_token=oov_tok)
+tokenizer = Tokenizer(num_words=vocab_size, oov_token=oov_tok, )
 tokenizer.fit_on_texts(input_seq)
 word_index = tokenizer.word_index
 model = tf.keras.models.load_model('sarcasm_detect.h5')
 
-sarcastic_sentences = [
-    "Wow, I love waking up early on weekends!",
-    "You're doing a great job breaking things.",
-    "Oh, because making things complicated is always the best approach.",
-    "Sure, let's prioritize adding more bugs to the codebase.",
-    "Oh, I absolutely love it when my computer crashes.",
-    "Yes, please make the font size smaller. That's what I need.",
-    "I'm thrilled to have another meeting scheduled for this afternoon.",
-    "Oh, fantastic! The internet is down again.",
-    "Brilliant idea! Let's reinvent the wheel.",
-    "You know what's better than efficient code? Slow code."
-]
-
-text = st.text_input("Enter Text:", placeholder=random.choice(sarcastic_sentences))
+text = st.text_input("Enter Text: ", placeholder='Write a text for detect sarcasm or not sarcasm')
 
 col2, col3 = st.columns(2)
 
@@ -70,11 +49,5 @@ def handle_input_text():
     else:
         col3.write("")
 
-# Create a selectbox for color palette
-selected_color = st.selectbox("Select Color:", list(color_palette.keys()))
 
-# Set the color of col3 based on the selected color
-col3 = st.color_picker("Result Color", color_palette[selected_color])
-
-# Apply the selected color to the button
 col2.button("Detect🔍", on_click=handle_input_text)
